5c1e531eba228e94f3fa6c8ce16f4fde285a13a9
angelRicardoSZ <angel.sanchezzefe@alumno.buap.mx>
2022-02-22 20:30:29 -0600

    Constructor method in Java and Js (class 23)

--- POO/UBER/JAVA/Main.java
+++ POO/UBER/JAVA/Main.java
@@ -1,6 +1,6 @@
 class Main {
     public static void main(String[] args) {
-        System.out.println("hola");
+        
         Car car = new Car();
         car.license = "AMQ123";
         car.driver = "Angel";
--- POO/UBER/PYTHON/main.py
+++ POO/UBER/PYTHON/main.py
@@ -1,7 +1,7 @@
 from car import Car
 
 if __name__=="__main__":
-    print("Hola Mundo");
+    
     car = Car()
     car.license = "acd123" 
     car.driver = "jus"
