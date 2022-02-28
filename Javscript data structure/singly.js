36ba88888bfadbbcf23b533e465b9af6f3dacd3f
angelRicardoSZ <angel.sanchezzefe@alumno.buap.mx>
2022-02-27 20:17:01 -0600

    Working with linked List: insert method

--- Javscript data structure/singly.js
+++ Javscript data structure/singly.js
@@ -1,18 +1,18 @@
-let sinlgyLinkedList = {
-    head: {
-        value: 1
-        next: {
-            value: 2
-            next: {
-                value: 3
-                next: {
-                    value: 4
-                    next: null
-                }
-            }
-        }
-    }
-}
+// let sinlgyLinkedList = {
+//     head: {
+//         value: 1
+//         next: {
+//             value: 2
+//             next: {
+//                 value: 3
+//                 next: {
+//                     value: 4
+//                     next: null
+//                 }
+//             }
+//         }
+//     }
+// }
 
 class Node {
     constructor(value){
@@ -31,7 +31,44 @@ class MySinglyLinkedList {
         this.tail = this.head;
         this.length = 1;
     }
+    append(value){
+        const newnode = new Node(value);
+        this.tail.next = newnode; // Altera tanto al tail.next como al head.next
+        this.tail = newnode;      // solo cambia el tail por el newnode
+        this.length++;
+        return this;
+    }
+    prepend(value){
+        const newNode = new Node(value);
+        newNode.next = this.head;
+        this.head = newNode;
+        this.length++;
+        return this;
+    }
+    insert(index,value){
+        if(index >= this.length){
+            return this.append(value)
+        }
+        const newNode = new Node(value);
+        const firstPointer  = this.getTheIndex(index-1);
+        const holdingPointer = firstPointer.next;
+        firstPointer.next = newNode;
+        newNode.next = holdingPointer;
 
+
+        this.length++;
+        return this;
+
+    }
+    getTheIndex(index) {
+        let counter = 0;
+        let currentNode = this.head;
+        while(counter !== index) {
+            currentNode =currentNode.next;
+            counter++;
+        }
+        return currentNode;
+    }
 }
 
 
